f8da6de475b8cc2bc6f3de341261c1fa578bd176
glycoknob <mt@i3o.de>
2017-01-19 14:57:59 +0100

    cleanup & update kalua.sh

--- files/etc/profile.d/kalua.sh
+++ files/etc/profile.d/kalua.sh
@@ -1,45 +1,135 @@
 #!/bin/sh
 
-# e.g. user@hostname:~
-export PS1='\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\] '
+prompt_set()
+{
+	face()
+	{
+		local rc=$?
 
-alias n='wget -qO - http://127.0.0.1:2006/neighbours'
+		case "$rc" in
+			0) printf '%s' "$1" ;;
+			*) printf '%s' "$2" ; return $rc ;;
+		esac
+	}
+
+	local e='\[\e'			# start escape-sequence
+	local c='\]'			# close escape-sequence
+
+	local user='\u'
+	local wdir='\w'			# workdir
+	local host='\h'			# short form
+
+	local reset="${e}[0m${c}"	# all attributes
+	local white="${e}[37m${c}"
+	local cyan="${e}[36m${c}"
+	local yellow="${e}[33;1m${c}"	# bold
+	local green="${e}[32m${c}"
+	local red="${e}[31m${c}"
+
+	local ok="${green}:)"
+	local bad="${red}8("
+
+	# e.g. user@hostname:~ :)
+	export PS1="${cyan}${user}$white@${green}$host:${yellow}$wdir \$( face '$ok' '$bad' ) $reset"
+}
+
+case "$PS1" in
+	*' face '*)
+		return 0
+	;;
+esac
+
+prompt_set
+
+alias n='_olsr txtinfo'
 alias n2='echo /nhdpinfo link | nc 127.0.0.1 2009'
 alias ll='ls -la'
 alias lr='logread'
 alias flush='_system ram_free flush'
 alias myssh='ssh -i $( _ssh key_public_fingerprint_get keyfilename )'
-alias regen='/etc/kalua_init; _(){ false;}; . /tmp/loader'
+alias regen='_ rebuild; _(){ false;}; . /tmp/loader'
+alias unload='_ u'
+alias dropshell='echo >>$SCHEDULER_IMPORTANT "/etc/init.d/dropbear stop"; killall dropbear'
+
+read -r LOAD <'/proc/loadavg'
+case "$LOAD" in
+	'0'*)
+	;;
+	*)
+		echo '[ATT] high load:'
+		uptime
+	;;
+esac
+unset LOAD
+
+read -r UP REST <'/proc/uptime'
+UP="${UP%.*}"
+case "${#UP}" in 1|2|3) echo "[ATT] low uptime: $UP sec";; esac
+unset UP REST
 
 case "$USER" in
-	'root')
+	'root'|'')
+		# FIXME! needs 'mkpasswd'
 		grep -qs ^"root:\$1\$b6usD77Q\$XPs6VECsQzFy9TUuQUAHW1:" '/etc/shadow' && {
-			echo "[ERROR] change weak root-password ('admin') with 'passwd'"
+			echo "[ERROR] change weak password ('admin') with 'passwd'"
 		}
 
 		grep -qs ^'root:\$' '/etc/shadow' || {
-			echo "[ERROR] set root-password with 'passwd'"
+			echo "[ERROR] unset password, use 'passwd'"
 		}
 	;;
 esac
 
-[ -e '/tmp/REBOOT_REASON' ] && {
-	read -r CRASH <'/tmp/REBOOT_REASON'
-	case "$CRASH" in
-		'nocrash'|'nightly_reboot'|'apply_settings'|'wifimac_safed')
-		;;
-		*)
-			# see system_crashreboot()
-			echo "last reboot unusual = '$CRASH', see with: cat /sys/kernel/debug/crashlog"
-		;;
-	esac
-	unset CRASH
-}
-
 _ t 2>/dev/null || {
 	[ -e '/tmp/loader' ] && {
-		. '/tmp/loader'
+		# http://unix.stackexchange.com/questions/82347/how-to-check-if-a-user-can-access-a-given-file
+		. '/tmp/loader'		# TODO: avoid "no permission" on debian user-X-session
+
 		echo
-		echo 'for some hints type: _help overview'
+		echo "this is a '$HARDWARE' - for some hints type: _help overview"
+
+		[ -n "$SSH_CONNECTION" -a -n "$WIFIDEV" ] && {
+			NAME="$( _wifi longshot_name )" && {
+				echo
+				echo "this device is part of a wifi-longshot named '$NAME'"
+				echo 'get stats with: _wifi longshot_report'
+			}
+
+			unload wifi
+			unset NAME
+		}
 	}
 }
+
+if   [ -e '/etc/init.d/apply_profile' -a -e '/sbin/uci' ]; then
+	echo "fresh/unconfigured device detected, run: '/etc/init.d/apply_profile.code' for help"
+elif [ -e '/tmp/REBOOT_REASON' ]; then
+	# see system_crashreboot()
+	read -r CRASH <'/tmp/REBOOT_REASON'
+	[ -e '/tmp/loader' ] && . /tmp/loader
+	_system include
+
+	case "$CRASH" in
+		'nocrash'|'nightly_reboot'|'apply_profile'|'wifimac_safed')
+			CRASH="$( _system reboots )"
+
+			test ${CRASH:-0} -gt 50 && {
+				echo "detected $CRASH reboots since last update - please check"
+			}
+		;;
+		*)
+			UNIXTIME=$( date +%s )
+			UPTIME=$( _system uptime sec )
+			printf '\n%s' "last reboot unusual @ $( date -d @$(( UNIXTIME - UPTIME )) ) - "
+
+			if [ -e '/sys/kernel/debug/crashlog' ]; then
+				printf '%s\n\n' "was: $CRASH, see with: cat /sys/kernel/debug/crashlog"
+			else
+				printf '%s\n\n' "was: $CRASH"
+			fi
+		;;
+	esac
+
+	unset CRASH UNIXTIME UPTIME
+	unload system
+fi
