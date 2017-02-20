7bbcdd68c069ea67804107eff724b0856387151d
mt <mt@i3o.de>
2017-02-20 02:07:50 +0100

    better hardware detection

--- files/etc/uci-defaults/00-set-hardware.sh
+++ files/etc/uci-defaults/00-set-hardware.sh
@@ -5,8 +5,12 @@ if [ ! -s /etc/HARDWARE ]; then
     [ -z "$HARDWARE" ] && { 
         HARDWARE=$(grep ^machine /proc/cpuinfo | sed 's/.*: \(.*\)/\1/')
     }
-    [ -z "$HARDWARE" ] && { 
-         HARDWARE="unknown-$( uname -m )" 
+    [ -z "$HARDWARE" ] && {
+    	if $(grep -qc ^"model name" /proc/cpuinfo); then 
+    		HARDWARE=$(uname -m)-$(grep ^"model name" /proc/cpuinfo | sed 's/.*: \(.*\)/\1/')
+    	elif 
+		HARDWARE=$(uname -m)-unknown
+    	fi
     }
     echo "$HARDWARE" > /etc/HARDWARE
 fi
