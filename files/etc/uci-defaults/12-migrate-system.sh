72f5e7c272c568b787abc715f7c996e62a03ee38
mt <mt@i3o.de>
2017-02-26 03:16:05 +0100

    cleanup system and migrate hostname,nodenumber etc.pp

--- files/etc/uci-defaults/12-migrate-system.sh
+++ files/etc/uci-defaults/12-migrate-system.sh
@@ -1,8 +1,39 @@
-#!/bin/sh 
+#!/bin/sh
 
 . /lib/functions.sh 
 
+config_cb() {
+    local type="$1"
+    local name="$2"
+    case "$type" in
+	profile) 
+        	option_cb() {                                            
+            		local option="$1" 
+            		local value="$2" 
+			uci_set meshnode settings "$option" "$value"
+        	}
+		uci_remove system profile
+	;;
+	system)
+		option_cb() {
+			local option="$1"
+			local value="$2"
+			[ "$option" = "hostname" ] && {
+				uci_set meshnode settings hostname "$value"
+			}
+		}
+		uci_remove system noswinstall 
+	;;
+        weblogin|monitoring|fwupdate|admin|vpn)  
+                uci_remove system "$type"
+        ;;
+    	*) 
+        	option_cb() { return; }
+	;;
+	esac
+}
 
-config_load system || return 0 
+config_load system
 
-# vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
+uci_commit
+
