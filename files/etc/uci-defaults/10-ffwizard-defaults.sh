eb2e5e775080ae24f626e1bbac49ecf1cc2c6e4f
mt <mt@i3o.de>
2017-11-11 00:41:46 +0100

    less is more - should fix #305

--- files/etc/uci-defaults/10-ffwizard-defaults.sh
+++ files/etc/uci-defaults/10-ffwizard-defaults.sh
@@ -2,15 +2,10 @@
 
 uci -m import ffwizard <<EOF
 config node 'settings'
-	option hostname ''
-	option nodenumber '-1'
 	option ipschema 'ffweimar'
 	option roaming '0'
 	option ipv6 '0'
 	option ip6prefix 'fd42:7ceb:f2ff::/48'
-	option location ''
-	option latitude ''
-	option longitutde ''
 
 EOF
 # vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
