7f983e6725e55e4d398b89972c335e0e753313fc
mt <mt@i3o.de>
2017-11-12 17:07:28 +0100

    always recreate our server list for now

--- files/etc/uci-defaults/99-vpn.sh
+++ files/etc/uci-defaults/99-vpn.sh
@@ -2,12 +2,17 @@
 
 . /lib/functions.sh
 
-[ -z $(uci_get network vtun_vpn keep) ] && {
-	uci_remove network vtun_vpn
-	uci -m import network <<-EOF
-	config vtun_vpn
-	        list endpoint '2.v.weimarnetz.de'
-	        list endpoint '3.v.weimarnetz.de'
-	EOF
-	uci commit 
-}
+#fixme
+
+while true; do 
+	uci delete network.@vtun_vpn[0]
+	[ "$?" -eq 0 ] || break
+	uci commit
+done 
+
+uci -m import network <<-EOF
+config vtun_vpn
+   list endpoint '2.v.weimarnetz.de'
+   list endpoint '3.v.weimarnetz.de'
+EOF
+uci commit 
