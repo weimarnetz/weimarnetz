ab84fbf908030b90c603cddeb9c6fc2ffa13b94c
mt <mt@i3o.de>
2017-12-02 16:11:07 +0100

    add more vpn server

--- files/etc/uci-defaults/99-vpn.sh
+++ files/etc/uci-defaults/99-vpn.sh
@@ -3,7 +3,6 @@
 . /lib/functions.sh
 
 #fixme
-
 while true; do 
 	uci delete network.@vtun_vpn[0]
 	[ "$?" -eq 0 ] || break
@@ -12,7 +11,11 @@ done
 
 uci -m import network <<-EOF
 config vtun_vpn
-   list endpoint '2.v.weimarnetz.de'
-   list endpoint '3.v.weimarnetz.de'
+	list endpoint '1.v.weimarnetz.de'
+	list endpoint '2.v.weimarnetz.de'
+	list endpoint '3.v.weimarnetz.de'
+	list endpoint '4.v.weimarnetz.de'
+	list endpoint '5.v.weimarnetz.de'
 EOF
-uci commit 
+uci commit
+# vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
