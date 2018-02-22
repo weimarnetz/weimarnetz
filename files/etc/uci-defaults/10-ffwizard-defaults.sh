3ff82257a21edcc7483439e839b25550e93927ba
mt <mt@i3o.de>
2018-02-22 01:19:27 +0100

    activate roaming

--- files/etc/uci-defaults/10-ffwizard-defaults.sh
+++ files/etc/uci-defaults/10-ffwizard-defaults.sh
@@ -3,7 +3,7 @@
 uci -m import ffwizard <<EOF
 config node 'settings'
 	option ipschema 'ffweimar'
-	option roaming '0'
+	option roaming '1'
 	option ipv6 '0'
 	option ip6prefix 'fd42:7ceb:f2ff::/48'
 
