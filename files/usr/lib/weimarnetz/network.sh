2e0082778f7be632c82db0bfbdae8026989a3ddc
mt <mt@i3o.de>
2018-02-27 01:38:31 +0100

    use uclient-fetch

--- files/usr/lib/weimarnetz/network.sh
+++ files/usr/lib/weimarnetz/network.sh
@@ -4,10 +4,10 @@
 net_http_get()
 {
 	local url="$1"
-	local max="${2:-15}"	# maximal running time [sec]
+	local max="${2:-1}"	# maximal running time [sec]
 
 	# log $funcname "max ${max}s, ${#url} bytes, wget -qO - '$url'"
-	timeout "$max" wget -qO - "$url" 
+	uclient-fetch -T "$max" -qO - "$url" 
 }
 
 net_tcp_port_reachable()
@@ -16,7 +16,7 @@ net_tcp_port_reachable()
 	local port="$2"			# e.g. 80
 
 	# we can't rely on '-w3' or '-z' because of lobotomized busybox netcat
-	timeout 5 echo "foo" | nc "$server" "$port" 1>/dev/null 2>/dev/null || return 1 
+	timeout 1 echo "foo" | nc "$server" "$port" 1>/dev/null 2>/dev/null || return 1 
 }
 
 # vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
