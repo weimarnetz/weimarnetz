30fa3fb42cb05daee8c5e7228a27826c66adaf83
mt <mt@i3o.de>
2018-03-14 05:33:09 +0100

    add settings for restrict and vpn blocking

--- files/etc/uci-defaults/10-ffwizard-defaults.sh
+++ files/etc/uci-defaults/10-ffwizard-defaults.sh
@@ -4,6 +4,8 @@ uci -m import ffwizard <<EOF
 config node 'settings'
 	option ipschema 'ffweimar'
 	option roaming '1'
+	option restrict '1'
+	option paranoia '1'
 	option ipv6 '0'
 	option ip6prefix 'fd42:7ceb:f2ff::/48'
 
