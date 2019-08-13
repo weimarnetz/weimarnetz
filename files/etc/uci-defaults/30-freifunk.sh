0563f08b9e3a43b29914c7b6d559973f697ae517
Andreas Bräu <andreas.braeu@idealo.de>
2019-08-13 21:37:33 +0200

    set mapserver to hopglass

--- files/etc/uci-defaults/30-freifunk.sh
+++ files/etc/uci-defaults/30-freifunk.sh
@@ -4,6 +4,6 @@
 
 uci_set freifunk community name "Weimar"
 uci_set freifunk community owm_api "http://mapapi.weimarnetz.de"
-uci_set freifunk community mapserver "http://map.weimarnetz.de"
+uci_set freifunk community mapserver "http://hopglass.weimarnetz.de"
 uci_set freifunk community homepage "http://weimarnetz.de"
 uci_set freifunk community registrator "http://reg.weimarnetz.de"
