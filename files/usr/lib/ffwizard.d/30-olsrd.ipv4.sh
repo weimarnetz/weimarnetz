ed9818d0d4d26b226db52bf982ca780ed7931ab7
Martin Tippmann <martin.tippmann@gmail.com>
2019-10-04 02:05:59 +0200

    implement a penalty for ibss and 2.4ghz links - if this works 11s and 5ghz links should be prefered

--- files/usr/lib/ffwizard.d/30-olsrd.ipv4.sh
+++ files/usr/lib/ffwizard.d/30-olsrd.ipv4.sh
@@ -24,6 +24,8 @@ setup_olsrbase() {
 	uci_set olsrd "$cfg" AllowNoInt "yes"
 	uci_set olsrd "$cfg" NatThreshold "0.75"
 	uci_set olsrd "$cfg" LinkQualityAlgorithm "etx_ffeth"
+	uci_set olsrd "$cfg" LinkQualityLevel "2"
+	uci_set olsrd "$cfg" LinkQualityFishEye "1"
 	uci_set olsrd "$cfg" FIBMetric "flat"
 	uci_set olsrd "$cfg" TcRedundancy "2"
 	uci_set olsrd "$cfg" Pollrate "0.025"
@@ -32,15 +34,15 @@ setup_olsrbase() {
 
 setup_InterfaceDefaults() {
 	uci_add olsrd InterfaceDefaults ; cfg="$CONFIG_SECTION"
-	uci_set olsrd "$cfg" MidValidityTime "500.0"
+	uci_set olsrd "$cfg" MidValidityTime "300.0"
 	uci_set olsrd "$cfg" TcInterval "2.0"
 	uci_set olsrd "$cfg" HnaValidityTime "125.0"
 	uci_set olsrd "$cfg" HelloValidityTime "125.0"
-	uci_set olsrd "$cfg" TcValidityTime "500.0"
+	uci_set olsrd "$cfg" TcValidityTime "300.0"
 	uci_set olsrd "$cfg" Ip4Broadcast "255.255.255.255"
 	uci_set olsrd "$cfg" MidInterval "25.0"
-	uci_set olsrd "$cfg" HelloInterval "3.0"
-	uci_set olsrd "$cfg" HnaInterval "10.0"
+	uci_set olsrd "$cfg" HelloInterval "2.0"
+	uci_set olsrd "$cfg" HnaInterval "5.0"
 }
 
 setup_Plugin_json() {
@@ -158,6 +160,7 @@ setup_wifi() {
 			uci_set olsrd "$iface_sec" interface "radio${idx}_mesh"
 			uci_set olsrd "$iface_sec" ignore "0"
 			uci_set olsrd "$iface_sec" Mode "mesh"
+			uci_set olsrd "$iface_sec" LinkQualityMult "default 0.75"
 			olsr_enabled=1
 
 			# 11s
@@ -166,6 +169,8 @@ setup_wifi() {
 			uci_set olsrd "$iface_sec" interface "radio${idx}_11s"
 			uci_set olsrd "$iface_sec" ignore "0"
 			uci_set olsrd "$iface_sec" Mode "mesh"
+			uci_set olsrd "$iface_sec" LinkQualityMult "default 0.90"
+
 			olsr_enabled=1
 
 			;;
