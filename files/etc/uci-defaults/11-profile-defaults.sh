e8b361a82b17547bc554126312c784526dd26b70
Martin Tippmann <martin.tippmann@gmail.com>
2019-10-05 18:26:29 +0200

    add profiles on first boot, we need the values for ffwizard and they might be missing

---
+++ files/etc/uci-defaults/11-profile-defaults.sh
@@ -0,0 +1,28 @@
+#!/bin/sh 
+
+uci -m import profile_Weimar <<EOF
+config community 'profile'
+        option 'name' 'Weimar'
+        option 'homepage' 'http://www.weimarnetz.de'
+        option 'ssid' 'Weimarnetz.Freifunk'
+        option 'mesh_ssid' 'mesh.%d.ch%d.weimarnetz.de'
+        option 'ap_ssid' 'weimarnetz.de | %s' 
+        option 'mesh_network' '10.63.0.0/16'
+        option 'latitude' '50.989530'
+        option 'longitude' '11.338675'
+EOF
+
+uci -m import profile_Camburg <<EOF
+config community 'profile'
+        option 'name' 'Camburg'
+        option 'homepage' 'http://camburg.freifunk.net'
+        option 'ssid' 'Camburg.Freifunk'
+        option 'mesh_ssid' 'mesh.%d.ch%d.camburg.ff'
+        option 'ap_ssid' 'Freifunk Camburg | %s'
+        option 'mesh_network' '10.63.0.0/16'
+        option 'latitude' '51.053889'
+        option 'longitude' '11.7075'
+EOF
+
+uci commit
+# vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
