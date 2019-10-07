ac3641dbde48dae5a4c593b8694a9cb5553d8d35
Martin Tippmann <martin.tippmann@gmail.com>
2019-10-07 04:02:26 +0200

    don't check for dead phys in the first 10 minutes of update - iw scan might cause trouble with detecting dfs

--- files/usr/sbin/reset_deaf_phys.sh
+++ files/usr/sbin/reset_deaf_phys.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # checks for deaf interfaces and brings them up, only if there are associated stations
 
+[ $(cut -d '.' -f1 /proc/uptime) -gt 600 ] || return 
+
 for num in 0 1; do
     if [ -d /sys/kernel/debug/ieee80211/phy$num/netdev\:wlan$num ]; then
+        [ -d ]
         logger "deaf_phys: checking wlan$num status..."
         neighs=$(ls -ld /sys/kernel/debug/ieee80211/phy$num/netdev\:wlan$num/stations/* 2>/dev/null | wc -l)
         if [ $neighs == 0 ]; then
