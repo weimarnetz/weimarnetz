db385198a214ec4fb6f91816918c5b0a4b1071a6
Andreas Bräu <ab@andi95.de>
2019-10-08 10:28:00 +0200

    preserve ffwizard setting on upgrade

--- files/etc/uci-defaults/10-ffwizard-defaults.sh
+++ files/etc/uci-defaults/10-ffwizard-defaults.sh
@@ -1,14 +1,15 @@
 #!/bin/sh 
 
-uci -m import ffwizard <<EOF
-config node 'settings'
-	option ipschema 'ffweimar'
-	option roaming '1'
-	option restrict '1'
-	option legacy '1'
-	option ipv6 '0'
-	option ip6prefix 'fd42:7ceb:f2ff::/48'
+. /lib/functions.sh 
 
-EOF
-uci commit
+uci_add ffwizard node "settings"
+
+uci_set ffwizard settings ipschema "$(uci_get ffwizard settings ipschema 'ffweimar')"
+uci_set ffwizard settings roaming "$(uci_get ffwizard settings roaming '1')"
+uci_set ffwizard settings restrict "$(uci_get ffwizard settings restirct '1')"
+uci_set ffwizard settings legacy "$(uci_get ffwizard settings legacy '1')"
+uci_set ffwizard settings ipv6 "$(uci_get ffwizard settings ipv6 '0')"
+uci_set ffwizard settings ipv6prefix "$(uci_get ffwizard settings ipv6prefix 'fd42:7ceb:f2ff::/48')"
+uci_commit ffwizard
+
 # vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
