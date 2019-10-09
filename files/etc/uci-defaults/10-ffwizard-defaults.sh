a580fc6876848614eda84cf02a6bba74940bf825
Andreas Bräu <ab@andi95.de>
2019-10-09 11:55:07 +0200

    fck tpo

--- files/etc/uci-defaults/10-ffwizard-defaults.sh
+++ files/etc/uci-defaults/10-ffwizard-defaults.sh
@@ -7,7 +7,7 @@ uci_add ffwizard node "settings"
 
 uci_set ffwizard settings ipschema "$(uci_get ffwizard settings ipschema 'ffweimar')"
 uci_set ffwizard settings roaming "$(uci_get ffwizard settings roaming '1')"
-uci_set ffwizard settings restrict "$(uci_get ffwizard settings restirct '1')"
+uci_set ffwizard settings restrict "$(uci_get ffwizard settings restrict '1')"
 uci_set ffwizard settings legacy "$(uci_get ffwizard settings legacy '1')"
 uci_set ffwizard settings ipv6 "$(uci_get ffwizard settings ipv6 '0')"
 uci_set ffwizard settings ipv6prefix "$(uci_get ffwizard settings ipv6prefix 'fd42:7ceb:f2ff::/48')"
