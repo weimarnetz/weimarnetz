c2eddacd4958d9b0e0b163deca95029962bd0972
Andi Bräu <andibraeu@users.noreply.github.com>
2020-04-21 21:38:21 +0200

    remove legacy settings working on #341

--- files/etc/uci-defaults/10-ffwizard-defaults.sh
+++ files/etc/uci-defaults/10-ffwizard-defaults.sh
@@ -8,10 +8,10 @@ uci_add ffwizard node "settings"
 uci_set ffwizard settings ipschema "$(uci_get ffwizard settings ipschema 'ffweimar')"
 uci_set ffwizard settings roaming "$(uci_get ffwizard settings roaming '1')"
 uci_set ffwizard settings restrict "$(uci_get ffwizard settings restrict '1')"
-uci_set ffwizard settings legacy "$(uci_get ffwizard settings legacy '0')"
 uci_set ffwizard settings blockWanPort "$(uci_get ffwizard settings block_wan_port '0')"
 uci_set ffwizard settings ipv6 "$(uci_get ffwizard settings ipv6 '0')"
 uci_set ffwizard settings ipv6prefix "$(uci_get ffwizard settings ipv6prefix 'fd42:7ceb:f2ff::/48')"
+uci_remove ffwizard settings legacy
 uci_commit ffwizard
 
 # vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
