1ba74931eaf7ce7bdf8a9c9e6fd516a88f07a036
Andreas Bräu <ab@andi95.de>
2020-09-22 20:39:38 +0200

    simplify cron expression for registrator call

--- files/etc/uci-defaults/99-registrator.sh
+++ files/etc/uci-defaults/99-registrator.sh
@@ -3,5 +3,5 @@
 test -f /etc/crontabs/root || touch /etc/crontabs/root
 SEED="$( dd if=/dev/urandom bs=2 count=1 2>&- | hexdump | if read line; then echo 0x${line#* }; fi )"
 MIN="$(( $SEED % 59 ))"
-crontab -l | grep -q "/usr/sbin/registrator heartbeat" || crontab -l | { cat; echo "$MIN * * * *	test -e /usr/sbin/registrator && /usr/sbin/registrator heartbeat"; } | crontab -
+crontab -l | grep -q "/usr/sbin/registrator heartbeat" || crontab -l | { cat; echo "$MIN * * * *	/usr/sbin/registrator heartbeat"; } | crontab -
 # vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
