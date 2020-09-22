501587c4ad120746968253b7f290e51336b739a6
Andreas Bräu <ab@andi95.de>
2020-09-22 20:45:07 +0200

    preserve community information

--- files/etc/uci-defaults/30-freifunk.sh
+++ files/etc/uci-defaults/30-freifunk.sh
@@ -2,8 +2,8 @@
 
 . /lib/functions.sh
 
-uci_set freifunk community name "Weimar"
+uci_set freifunk community name "$(uci_get freifunk community name 'Weimar')"
 uci_set freifunk community owm_api "http://mapapi.weimarnetz.de"
 uci_set freifunk community mapserver "http://hopglass.weimarnetz.de"
-uci_set freifunk community homepage "http://weimarnetz.de"
+uci_set freifunk community homepage "$(uci_get freifunk community homepage 'https://weimarnetz.de')"
 uci_set freifunk community registrator "http://reg.weimarnetz.de"
