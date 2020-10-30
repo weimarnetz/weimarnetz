fc245e417d708879942bf9696a8a7844acb2a3ab
Andreas Bräu <ab@andi95.de>
2020-10-30 16:06:36 +0100

    set weimar as default if Freifunk was previously set

--- files/etc/uci-defaults/30-freifunk.sh
+++ files/etc/uci-defaults/30-freifunk.sh
@@ -2,8 +2,15 @@
 
 . /lib/functions.sh
 
-uci_set freifunk community name "$(uci_get freifunk community name 'Weimar')"
+community_name="$(uci_get freifunk community name 'Weimar')"
+website="$(uci_get freifunk community homepage 'https://weimarnetz.de')"
+if [ "$community_name" = "Freifunk" ]; then
+  community_name="Weimar"
+  website="https://weimarnetz.de"
+fi
+
+uci_set freifunk community name "$community_name"
 uci_set freifunk community owm_api "http://mapapi.weimarnetz.de"
 uci_set freifunk community mapserver "http://hopglass.weimarnetz.de"
-uci_set freifunk community homepage "$(uci_get freifunk community homepage 'https://weimarnetz.de')"
+uci_set freifunk community homepage "$website"
 uci_set freifunk community registrator "http://reg.weimarnetz.de"
