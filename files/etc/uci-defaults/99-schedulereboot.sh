7fa1869bff4aa6d32c2cc1ab60ef5ce5133cb9ac
Andreas Bräu <ab@andi95.de>
2021-01-19 18:16:29 +0100

    add monthly reboot

---
+++ files/etc/uci-defaults/99-schedulereboot.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+# shellcheck disable=SC2039
+test -f /etc/crontabs/root || touch /etc/crontabs/root
+SEEDMIN="$( dd if=/dev/urandom bs=2 count=1 2>&- | hexdump | if read line; then echo 0x${line#* }; fi )"
+SEEDDAY="$( dd if=/dev/urandom bs=2 count=1 2>&- | hexdump | if read line; then echo 0x${line#* }; fi )"
+MIN="$(( $SEEDMIN % 59 ))"
+DAY="$(( $SEEDDAY % 59 ))"
+crontab -l | grep -q "reboot" || crontab -l | { cat; echo "$MIN 3 $DAY * *	reboot"; } | crontab -
+# vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
