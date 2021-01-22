3a75637edcff6b7efca94114bf6f9568babf2b83
Andreas Bräu <ab@andi95.de>
2021-01-22 09:50:57 +0100

    fix random day generation

--- files/etc/uci-defaults/99-schedulereboot.sh
+++ files/etc/uci-defaults/99-schedulereboot.sh
@@ -4,6 +4,6 @@ test -f /etc/crontabs/root || touch /etc/crontabs/root
 SEEDMIN="$( dd if=/dev/urandom bs=2 count=1 2>&- | hexdump | if read line; then echo 0x${line#* }; fi )"
 SEEDDAY="$( dd if=/dev/urandom bs=2 count=1 2>&- | hexdump | if read line; then echo 0x${line#* }; fi )"
 MIN="$(( $SEEDMIN % 59 ))"
-DAY="$(( $SEEDDAY % 59 ))"
+DAY="$(( $SEEDDAY % 28 ))"
 crontab -l | grep -q "/sbin/reboot" || crontab -l | { cat; echo "$MIN 3 $DAY * *	/sbin/reboot"; } | crontab -
 # vim: set filetype=sh ai noet ts=4 sw=4 sts=4 :
