23abe6e572ef8169b3bdb3129da82a8d2bc632de
Andreas Bräu <ab@andi95.de>
2022-02-26 22:53:00 +0100

    remove global roaming setting

--- files/etc/uci-defaults/10-ffwizard-defaults.sh
+++ files/etc/uci-defaults/10-ffwizard-defaults.sh
@@ -6,11 +6,11 @@ uci -m import ffwizard < /dev/null
 uci_add ffwizard node "settings"
 
 uci_set ffwizard settings ipschema "$(uci_get ffwizard settings ipschema 'ffweimar')"
-uci_set ffwizard settings roaming "$(uci_get ffwizard settings roaming '1')"
 uci_set ffwizard settings restrict "$(uci_get ffwizard settings restrict '1')"
 uci_set ffwizard settings blockWanPort "$(uci_get ffwizard settings block_wan_port '0')"
 uci_set ffwizard settings ipv6 "$(uci_get ffwizard settings ipv6 '0')"
 uci_set ffwizard settings ipv6prefix "$(uci_get ffwizard settings ipv6prefix 'fd42:7ceb:f2ff::/48')"
+uci_remove ffwizard settings roaming
 uci_remove ffwizard settings legacy
 uci_commit ffwizard
 
